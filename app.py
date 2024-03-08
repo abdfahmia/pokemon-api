f32f0f5bd4647099c70e2bdfad6ca818f4f09021
PRINCE ALVIN YUSUF <princealvinyusuf@gmail.com>
2024-03-08 07:47:25 +0700

    Define Data Model of Pokemon Adjust UI in main.html to show the card view of pokemon

--- app.py
+++ app.py
@@ -4,10 +4,40 @@ import requests
 # Initialization
 app = Flask(__name__)
 
+# Define Model
+class Pokemon:
+    def __init__(self, img_url, name, number):
+        self.img_url = img_url
+        self.name = name
+        self.number = number
+
 # Handler in main controller
 @app.route('/')
 def main():
-    return render_template('main.html')
+    pokemons = [
+        Pokemon(number=1, name="hariyama", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/297.png" ),
+        Pokemon(number=2, name="porygon", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/137.png" ),
+        Pokemon(number=3, name="golduck", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/55.png" ),
+        Pokemon(number=4, name="magby", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/240.png" ),
+        Pokemon(number=5, name="pinsir", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/127.png" ),
+        Pokemon(number=6, name="ledian", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/166.png" ),
+        Pokemon(number=7, name="sceptile", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/254.png" ),
+        Pokemon(number=8, name="porygon", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/137.png" ),
+        Pokemon(number=9, name="pineco", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/204.png" ),
+        Pokemon(number=10, name="lickitung", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/108.png" ),
+        Pokemon(number=11, name="jynx", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/124.png" ),
+        Pokemon(number=12, name="hitmontop", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/237.png" ),
+        Pokemon(number=13, name="raichu", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/26.png" ),
+        Pokemon(number=14, name="houndour", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/228.png" ),
+        Pokemon(number=15, name="unown", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/201.png" ),
+        Pokemon(number=16, name="houndoom", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/229.png" ),
+        Pokemon(number=17, name="azurill", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/298.png" ),
+        Pokemon(number=18, name="sentret", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/161.png" ),
+        Pokemon(number=19, name="jynx", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/124.png" ),
+        Pokemon(number=20, name="loudred", img_url="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/294.png" ),
+    ] 
+    return render_template("main.html", pokemons=pokemons, number=len(pokemons))
+
 
 # Handler action if /result is triggered
 @app.route('/result', methods=['GET', 'POST'])
